23284ba9c381cee5a094767c440ce5b76032af6e
mikhail-j <mikhail.jin@gmail.com>
2017-01-30 21:23:51 -0500

    Added separate appoximation functions for edge lengths and radius approximation.

--- polyangle_ctx_2.js
+++ polyangle_ctx_2.js
@@ -101,45 +101,103 @@ function getMax(an_array) {
 	}
 }
 
+function norm_vec2(ax, bx, ay, by) {
+	return Math.sqrt(((ax - bx) * (ax - bx)) + ((ay - by) * (ay - by)))
+}
+
+function dynamicEdgeApproximation(cr, ca, v0, v1) {
+	//var edge_dist = magnitude_vec2(new AnchorPoint(PV[i].x - PV[i-1].x, PV[i].y - PV[i-1].y));
+	var edge_dist = norm_vec2(v0.x, v1.x, v0.y, v1.y);
+	var edge_angle = 0.0;
+	var nx = cr * Math.cos(edge_angle + ca);
+	var ny = cr * Math.sin(edge_angle + ca);
+	var new_edge_dist = norm_vec2(nx, (cr * Math.cos(ca)), ny, (cr * Math.sin(ca)));
+
+	var stepsize = Math.PI * (1.0/360.0);
+	var negative;
+
+	if ((edge_dist - new_edge_dist) < 0) {			//the edge approximation went over the actual length
+		negative = -1;
+	}
+	else {
+		negative = 1;
+	}
+
+	while (!(Math.abs(edge_dist - new_edge_dist) < 0.001) && (2 * cr) >= edge_dist) {
+		//edge_angle = edge_angle + 0.00001;
+		edge_angle = edge_angle + (stepsize * negative);
+		nx = cr * Math.cos(edge_angle + ca);
+		ny = cr * Math.sin(edge_angle + ca);
+		new_edge_dist = norm_vec2(nx, (cr * Math.cos(ca)), ny, (cr * Math.sin(ca)));
+		if ((edge_dist - new_edge_dist) < 0) {			//the edge approximation went over the actual length
+			if (negative !== -1) {
+				stepsize = stepsize * (1.0/60.0);
+			}
+			negative = -1;
+		}
+		else {
+			if (negative !== 1) {
+				stepsize = stepsize * (1.0/60.0);
+			}
+			negative = 1;
+		}
+		//console.log("edge angle: " + edge_angle + " edge difference: " + (new_edge_dist - edge_dist));
+	}
+	//console.log("edge angle: " + edge_angle + " edge difference: " + (new_edge_dist - edge_dist));
+	if ((2 * cr) < edge_dist) {
+		edge_angle = 4 * Math.PI;
+	}
+	return edge_angle;
+}
+
+function dynamicCircleApproximation() {
+	incrementEdge();
+
+	var stepsize = 10;
+	var negative;
+
+	if (((2 * Math.PI) - getMax(vertexAngles)) < 0) {		//the circle radius is too small for our polygon
+		negative = -1;
+	}
+	else {
+		negative = 1;
+	}
+
+	while ((Math.abs((2 * Math.PI) - getMax(vertexAngles)) > 0.015) && circle_radius > 1) {
+		circle_radius = circle_radius - (stepsize * negative);
+		incrementEdge();
+		if (((2 * Math.PI) - getMax(vertexAngles)) < 0) {		//the circle radius is too small for our polygon
+			if (negative !== -1) {
+				stepsize = stepsize * 0.5;
+			}
+			negative = -1;
+		}
+		else {
+			if (negative !== 1) {
+				stepsize = stepsize * 0.5;
+			}
+			negative = 1;
+		}
+		console.log("angle missing: " + ((2 * Math.PI) - getMax(vertexAngles)) + " radius: " + circle_radius);
+	}
+	console.log("out of loop! radius: " + circle_radius);
+}
+
 function incrementEdge() {
 	central_angle = 0.0;
 	vertexAngles = [0.0];
 	edgeLengths = [];
 	for (var i = 1; i < PV.length; i++) {
-		var edge_dist = magnitude_vec2(new AnchorPoint(PV[i].x - PV[i-1].x, PV[i].y - PV[i-1].y));
-		var edge_angle = 0.0;
-		var nx = circle_radius * Math.cos(edge_angle + central_angle);
-		var ny = circle_radius * Math.sin(edge_angle + central_angle);
-		var new_edge_dist = magnitude_vec2(new AnchorPoint(nx - (circle_radius * Math.cos(central_angle)), ny - (circle_radius * Math.sin(central_angle))));
-		while (!(Math.abs(edge_dist - new_edge_dist) < 0.5)) {
-			edge_angle = edge_angle + 0.00001;
-			nx = circle_radius * Math.cos(edge_angle + central_angle);
-			ny = circle_radius * Math.sin(edge_angle + central_angle);
-			new_edge_dist = magnitude_vec2(new AnchorPoint(nx - (circle_radius * Math.cos(central_angle)), ny - (circle_radius * Math.sin(central_angle))));
-			//console.log("edge angle: " + edge_angle + " edge difference: " + (new_edge_dist - edge_dist));
-		}
-		//console.log("old edge: " + edge_dist + " new edge: " + new_edge_dist);
+		var edge_angle = dynamicEdgeApproximation(circle_radius, central_angle, PV[i], PV[i-1]);
 		central_angle = central_angle + edge_angle;
 		vertexAngles.push(central_angle);
-		edgeLengths.push(edge_dist);
+		edgeLengths.push(norm_vec2(PV[i].x, PV[i-1].x, PV[i].y, PV[i-1].y));
 		//drawEdges();
 	}
-	var edge_dist = magnitude_vec2(new AnchorPoint(PV[0].x - PV[PV.length - 1].x, PV[0].y - PV[PV.length - 1].y));
-	var edge_angle = 0.0;
-	var nx = circle_radius * Math.cos(edge_angle + central_angle);
-	var ny = circle_radius * Math.sin(edge_angle + central_angle);
-	var new_edge_dist = magnitude_vec2(new AnchorPoint(nx - (circle_radius * Math.cos(central_angle)), ny + (circle_radius * Math.sin(central_angle))));
-	while (!(Math.abs(edge_dist - new_edge_dist) < 0.5)) {
-		edge_angle = edge_angle + 0.00001;
-		nx = circle_radius * Math.cos(edge_angle + central_angle);
-		ny = circle_radius * Math.sin(edge_angle + central_angle);
-		new_edge_dist = magnitude_vec2(new AnchorPoint(nx - (circle_radius * Math.cos(central_angle)), ny + (circle_radius * Math.sin(central_angle))));
-		//console.log("edge angle: " + edge_angle + " edge difference: " + (new_edge_dist - edge_dist));
-	}
-	//console.log("old edge: " + edge_dist + " new edge: " + new_edge_dist);
+	var edge_angle = dynamicEdgeApproximation(circle_radius, central_angle, PV[0], PV[PV.length - 1]);
 	central_angle = central_angle + edge_angle;
 	vertexAngles.push(central_angle);
-	edgeLengths.push(edge_dist);
+	edgeLengths.push(norm_vec2(PV[0].x, PV[PV.length-1].x, PV[0].y, PV[PV.length-1].y));
 	//drawEdges();
 }
 
@@ -160,25 +218,7 @@ function minimizeCircle() {
 			radiusDamage = false;
 		}
 		else {
-			incrementEdge();
-			//while ((Math.abs((2 * Math.PI) - getMax(vertexAngles)) > 0.11 || ((2 * Math.PI) - getMax(vertexAngles)) < 0) && circle_radius > 2) {
-			while ((Math.abs((2 * Math.PI) - getMax(vertexAngles)) > 0.02) && circle_radius > 2) {
-				if ((Math.abs((2 * Math.PI) - getMax(vertexAngles)) > 0.4)) {
-					circle_radius = circle_radius - 0.2;
-				}
-				else if ((Math.abs((2 * Math.PI) - getMax(vertexAngles)) > 0.2)) {
-					circle_radius = circle_radius - 0.1;
-				}
-				else {
-					circle_radius = circle_radius - 0.01;
-				}
-				incrementEdge();
-				if ((Math.abs((2 * Math.PI) - getMax(vertexAngles)) < 0.02 && ((2 * Math.PI) - getMax(vertexAngles)) > 0)) {
-					break;
-				}
-				console.log("angle missing: " + ((2 * Math.PI) - getMax(vertexAngles)) + " radius: " + circle_radius);
-			}
-			console.log("out of loop!");
+			dynamicCircleApproximation();
 			renderSecondCtxFigure();
 			radiusDamage = false;
 		}
